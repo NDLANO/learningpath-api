406d5434cfb067c43871d03e698644594f6eb182
Kenneth Stigen <kes@knowit.no>
2016-06-29 11:47:51 +0200

    NDLA-276: Refaktorert og skrevet test for migrering

--- src/main/scala/db/migration/V3__ChangeTagStructure.scala
+++ src/main/scala/db/migration/V3__ChangeTagStructure.scala
@@ -12,33 +12,44 @@ import scalikejdbc._
 
 class V3__ChangeTagStructure extends JdbcMigration {
 
-  case class OldTag(tag: String, language: Option[String])
-
   implicit val formats = org.json4s.DefaultFormats
 
-  override def migrate(connection: Connection): Unit = {
+  override def migrate(connection: Connection) = {
     val db = DB(connection)
     db.autoClose(false)
 
     db.withinTx { implicit session =>
-      val all = sql"select id, document from learningpaths".map(rs => (rs.long("id"), rs.string("document"))).list().apply()
-      all.foreach(tuppel => {
-        val json = parse(tuppel._2)
-        val tags = json \\ "tags"
-
-        val oldTagsOpt: Option[List[OldTag]] = tags.extractOpt[List[OldTag]]
-        oldTagsOpt.foreach(oldTags => {
-          val newTags = oldTags.groupBy(_.language).map(entry => (entry._1, entry._2.map(_.tag))).map(entr => LearningPathTags(entr._2, entr._1))
-
-          val newJson = json.replace(List("tags"), parse(write(newTags)))
-
-          val dataObject = new PGobject()
-          dataObject.setType("jsonb")
-          dataObject.setValue(compact(render(newJson)))
-
-          sql"update learningpaths set document = $dataObject where id = ${tuppel._1}".update().apply
-        })
-      })
+      allLearningPaths.flatMap(convertTagsToNewFormat).foreach(update)
     }
   }
+
+  def allLearningPaths(implicit session: DBSession): List[DBLearningPath] = {
+    sql"select id, document from learningpaths".map(rs => DBLearningPath(rs.long("id"), rs.string("document"))).list().apply()
+  }
+
+  def convertTagsToNewFormat(learningPath: DBLearningPath): Option[DBLearningPath] = {
+    val json = parse(learningPath.document)
+    val tags = json \\ "tags"
+
+    val oldTagsOpt: Option[List[OldTag]] = tags.extractOpt[List[OldTag]]
+    oldTagsOpt match {
+      case None => None
+      case Some(oldTags) => {
+        val newTags = oldTags.groupBy(_.language).map(entry => (entry._1, entry._2.map(_.tag))).map(entr => LearningPathTags(entr._2, entr._1))
+        Some(learningPath.copy(document = compact(render(json.replace(List("tags"), parse(write(newTags)))))))
+      }
+    }
+  }
+
+  def update(learningPath: DBLearningPath)(implicit session: DBSession) = {
+    val dataObject = new PGobject()
+    dataObject.setType("jsonb")
+    dataObject.setValue(learningPath.document)
+
+    sql"update learningpaths set document = $dataObject where id = ${learningPath.id}".update().apply
+  }
 }
+
+case class OldTag(tag: String, language: Option[String])
+
+case class DBLearningPath(id: Long, document: String)
