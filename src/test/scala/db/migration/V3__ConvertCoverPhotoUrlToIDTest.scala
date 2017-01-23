107d81d50de3775cfdbe71446880f6938804d5bf
Henrik Nårstad <henrik.narstad@knowit.no>
2017-01-23 12:08:16 +0100

    #158: migration script to convert cover-photo url to id

---
+++ src/test/scala/db/migration/V3__ConvertCoverPhotoUrlToIDTest.scala
@@ -0,0 +1,30 @@
+package db.migration
+
+import no.ndla.learningpathapi.UnitSuite
+
+class V3__ConvertCoverPhotoUrlToIDTest extends UnitSuite {
+  val migration = new V3__ConvertCoverPhotoUrlToID()
+
+  test("That convertCoverPhotoUrl with no coverPhoto returns None") {
+    val learningPath = V3_DBLearningPath(1,"""{"tags":[]}""")
+    val optConverted = migration.convertCoverPhotoUrl(learningPath)
+
+    optConverted should be(None)
+  }
+
+  test("That converting an already converted learningPath returns None") {
+    val learningPath = V3_DBLearningPath(2,"""{"coverPhotoId": "1"}""")
+    migration.convertCoverPhotoUrl(learningPath) should be(None)
+  }
+
+  test("That convertCoverPhotoUrl converts to expected format") {
+    val before = """{"coverPhotoMetaUrl": "http://localhost/image-api/v1/images/55"}"""
+    val expectedAfter = """{"coverPhotoId":"55"}"""
+    val learningPath = V3_DBLearningPath(3, before)
+
+    val optConverted = migration.convertCoverPhotoUrl(learningPath)
+    optConverted.isDefined should be(true)
+    optConverted.get.document should equal(expectedAfter)
+  }
+
+}
