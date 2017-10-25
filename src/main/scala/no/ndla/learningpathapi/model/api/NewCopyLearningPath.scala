a7e9aa796e94ba88431b8bd0e600a41a7ae0484e
Jonas Natten <jonas.natten@knowit.no>
2017-10-25 09:10:33 +0200

    #646: Removes v1 versions of UpdatedLearningPath and NewCopyLearningPath

--- src/main/scala/no/ndla/learningpathapi/model/api/NewCopyLearningPath.scala
+++ src/main/scala/no/ndla/learningpathapi/model/api/NewCopyLearningPath.scala
@@ -14,14 +14,6 @@ import org.scalatra.swagger.runtime.annotations.ApiModelProperty
 import scala.annotation.meta.field
 
 @ApiModel(description = "Meta information for a new learningpath based on a copy")
-case class NewCopyLearningPath(@(ApiModelProperty@field)(description = "The titles of the learningpath") title: Seq[Title],
-                               @(ApiModelProperty@field)(description = "The descriptions of the learningpath") description: Seq[Description],
-                               @(ApiModelProperty@field)(description = "Url to cover-photo in NDLA image-api.") coverPhotoMetaUrl: Option[String],
-                               @(ApiModelProperty@field)(description = "The duration of the learningpath in minutes. Must be greater than 0") duration: Option[Int],
-                               @(ApiModelProperty@field)(description = "Searchable tags for the learningpath") tags: Seq[LearningPathTags],
-                               @(ApiModelProperty@field)(description = "Describes the copyright information for the learningpath") copyright: Option[Copyright])
-
-@ApiModel(description = "Meta information for a new learningpath based on a copy")
 case class NewCopyLearningPathV2(@(ApiModelProperty@field)(description = "The titles of the learningpath") title: String,
                                  @(ApiModelProperty@field)(description = "The descriptions of the learningpath") description: Option[String],
                                  @(ApiModelProperty@field)(description = "The chosen language") language: String,
