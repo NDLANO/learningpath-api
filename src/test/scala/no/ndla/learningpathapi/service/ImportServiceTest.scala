37ba86ef4b17758c2f00d2a0fe2a9b1b66da71c8
Christian Peter <christian150790@gmail.com>
2017-11-29 11:58:02 +0100

    Renames tests

--- src/test/scala/no/ndla/learningpathapi/service/ImportServiceTest.scala
+++ src/test/scala/no/ndla/learningpathapi/service/ImportServiceTest.scala
@@ -132,10 +132,10 @@ class ImportServiceTest extends UnitSuite with UnitTestEnvironment {
     importService.mapOldToNewLicenseKey(Some("noc")) should be(Some("pd"))
   }
 
-  test("That mapOldToNewLicenseKey converts nolaw to cc0") {
+  test("That mapOldToNewLicenseKey does not convert an license that should not be converted") {
     importService.mapOldToNewLicenseKey(Some("by-sa")) should be(Some("by-sa"))
   }
-  
+
   private def packageWithNodeId(nid: Long): Package = Package(nid, nid, "nb", "NodeTitle", None, "NodeDescription", 1, new Date(), 1, "PackageTittel", 1, 1, Seq(stepWithEmbedUrlAndLanguage(Some("http://ndla.no/node/12345"), "nb")))
   private def stepWithDescriptionAndLanguage(description: Option[String], language: String): Step = Step(1, 1, 1, "Tittel", 1, 1, None, description, None, language)
   private def stepWithEmbedUrlAndLanguage(embedUrl: Option[String], language: String): Step = Step(1, 1, 1, "Tittel", 1, 1, embedUrl, None, None, language)
